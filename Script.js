cd1b4388a13e07c3cb650027d11fd9e4a054f086
abigailsluv <61960948+abigailsluv@users.noreply.github.com>
2020-03-27 14:13:14 +0100

    aassignment

---
+++ Script.js
@@ -0,0 +1,7 @@
+var name = "abigail";
+var courses = ["HTML", "CSS", "and", "javascript"]
+const data = [] ;
+for (var i = 1; i < 200; i += 2) { data.push(i) }
+console.log (my name is abigail awofadeju)
+console.log (i am a frontend developer)
+console.log (data) 
